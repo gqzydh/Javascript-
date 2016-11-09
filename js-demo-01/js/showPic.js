ff9807076806eda9c144836825b097c65c5735b8
unknown <guoqing@jbnz.com>
2016-11-09 10:37:34 +0800

    js-demo-01编辑完成

--- js-demo-01/js/showPic.js
+++ js-demo-01/js/showPic.js
@@ -4,12 +4,6 @@ function showPic(whichpic) {
 		placeholder.setAttribute("src", source);
 		var text = whichpic.getAttribute("title");
 		var description = document.getElementById("description");
-		alert(description.childNodes[0].nodeValue);
-
+		description.firstChild.nodeValue = text;
 
 	}
-function countBodyChildren(){
-	var body_element = document.getElementsByTagName("body")[0];
-	alert(body_element.nodeType);
-}
-window.onload = countBodyChildren;
